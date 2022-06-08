704de68b3e193b879774a3396216442ec7800b7e
NpJunio <104942634+NpJunio@users.noreply.github.com>
2022-06-07 21:33:15 -0300

    Add files via upload

---
+++ Exer4Lista4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+main()
+{
+	setlocale(LC_ALL,"");
+	int numero, raizI;
+	float raiz;
+	cout<<"\n\nDigite um número inteiro ";//entre com o numero
+		cin>>numero;//receba o numero
+			raiz=sqrt(numero);//faça a raiz quadrada
+	raizI=sqrt(numero);//faça a raiz quadrada
+		if(raizI==raiz)//se os dois forem iguais
+	cout<<"\n\nÉ um quadrado perfeito \n\n";
+else
+cout<<"\n\nNão é um quadrado perfeito \n\n";
+}
