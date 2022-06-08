d17c0948a0e4246558528a51642aa9ded9a735ef
NpJunio <104942634+NpJunio@users.noreply.github.com>
2022-06-07 21:37:21 -0300

    Update Exer4Lista4.cpp
    mudei dados

--- Exer4Lista4.cpp
+++ Exer4Lista4.cpp
@@ -6,12 +6,12 @@ main()
 	setlocale(LC_ALL,"");
 	int numero, raizI;
 	float raiz;
-	cout<<"\n\nDigite um número inteiro ";//entre com o numero
+	cout<<"\n\nDigite um nÃºmero inteiro ";//entre com o numero
 		cin>>numero;//receba o numero
-			raiz=sqrt(numero);//faça a raiz quadrada
-	raizI=sqrt(numero);//faça a raiz quadrada
+			raiz=sqrt(numero);//faÃ§a a raiz quadrada
+	raizI=sqrt(numero);//faÃ§a a raiz quadrada
 		if(raizI==raiz)//se os dois forem iguais
-	cout<<"\n\nÉ um quadrado perfeito \n\n";
+	cout<<"\n\nÃ‰ um quadrado perfeito \n\n";
 else
-cout<<"\n\nNão é um quadrado perfeito \n\n";
-}
+cout<<"\n\nNÃ£o Ã© um quadrado perfeito \n\n";
+} 
